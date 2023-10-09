57434d946d841c6fb1b65aa8d365273eae4ca2ba
RayoKh <platypus1703@gmail.com>
2023-10-09 14:16:45 +0300

    updated by prettier

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,16 +1,26 @@
 import { Component } from 'react';
 import { Formik } from 'formik';
-import { StyledForm, SubmitBtn, StyledLabel, StyledField, ErrorMsg } from "./ContactForm.styled";
+import {
+  StyledForm,
+  SubmitBtn,
+  StyledLabel,
+  StyledField,
+  ErrorMsg,
+} from './ContactForm.styled';
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object().shape({
   name: Yup.string()
-    .matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
-      'Please check that the name you have dialed is correct')
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      'Please check that the name you have dialed is correct'
+    )
     .required('Required!'),
   number: Yup.string()
-    .matches(/\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}/,
-      'Please check that the number you have dialed is correct')
+    .matches(
+      /\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}/,
+      'Please check that the number you have dialed is correct'
+    )
     .required('Required!'),
 });
 
@@ -22,29 +32,29 @@ export class ContactForm extends Component {
 
   render() {
     return (
-        <Formik
-          initialValues={{ name: '', number: '' }}
-          validationSchema={validationSchema}
-          onSubmit={this.handleSubmit}
-        >
-          {({ isSubmitting }) => (
-            <StyledForm>
-              <StyledLabel>
-                Name
-                <StyledField type="text" id="name" name="name" required />
-                <ErrorMsg name="name" component="div" className="error" />
-              </StyledLabel>
-              <StyledLabel>
-                Number
-                <StyledField type="tel" id="number" name="number" required />
-                <ErrorMsg name="number" component="div" className="error" />
-              </StyledLabel>
-              <SubmitBtn type="submit" disabled={isSubmitting}>
-                Add contact
-              </SubmitBtn>
-            </StyledForm>
-          )}
-        </Formik>
+      <Formik
+        initialValues={{ name: '', number: '' }}
+        validationSchema={validationSchema}
+        onSubmit={this.handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <StyledForm>
+            <StyledLabel>
+              Name
+              <StyledField type="text" id="name" name="name" required />
+              <ErrorMsg name="name" component="div" className="error" />
+            </StyledLabel>
+            <StyledLabel>
+              Number
+              <StyledField type="tel" id="number" name="number" required />
+              <ErrorMsg name="number" component="div" className="error" />
+            </StyledLabel>
+            <SubmitBtn type="submit" disabled={isSubmitting}>
+              Add contact
+            </SubmitBtn>
+          </StyledForm>
+        )}
+      </Formik>
     );
   }
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,22 @@
-import { ContactItem, ListItem } from './ContactList.styled'
+import { ContactItem, ListItem } from './ContactList.styled';
 
 const Contact = ({ contact, onDeleteContact }) => (
   <ListItem>
-    <ContactItem>{contact.name}: {contact.number}</ContactItem>
+    <ContactItem>
+      {contact.name}: {contact.number}
+    </ContactItem>
     <button onClick={() => onDeleteContact(contact.id)}>Видалити</button>
   </ListItem>
 );
 
 export const ContactList = ({ contacts, onDeleteContact }) => (
-    <ul>
-      {contacts.map((contact) => (
-        <Contact key={contact.id} contact={contact} onDeleteContact={onDeleteContact} />
-      ))}
-    </ul>
+  <ul>
+    {contacts.map(contact => (
+      <Contact
+        key={contact.id}
+        contact={contact}
+        onDeleteContact={onDeleteContact}
+      />
+    ))}
+  </ul>
 );
